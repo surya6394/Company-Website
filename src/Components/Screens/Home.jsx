fe6a16fc4dec2bc7f7ca049fce9d6785a87520c3
SuryaPratap-ClevDoc <suryapratap@clevdoc.com>
2024-07-13 00:08:33 +0530

    Added Tailwind , created folder structure

---
+++ src/Components/Screens/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Elements/Navbar'
+
+const Home = () => {
+  return (
+    <>
+    This is Home Page.
+        <Navbar />
+    </>
+  )
+}
+
+export default Home
